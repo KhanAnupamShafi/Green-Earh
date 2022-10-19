d1376c475b83b4214d0f145a15987d930685e3f9
ali <aliakbar31195@gmail.com>
2022-10-19 18:46:37 +0600

    rules

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
       <h1>Hello World !</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>I am Rafsan from mirpur to test the project as a collaborator</p>
+      
       <h2>good to see you</h2>
+      <h2>setting new rules  for git hub pushing</h2>
     </div>
   );
 }
